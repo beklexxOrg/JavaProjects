13cd5b173716c3544e55b52acbddbb78a099041c
beklexx <beklexx@gmail.com>
2023-03-27 17:11:04 +0500

    First app

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 34, 6, 78, 8, 9, 32, 1, 34};
+        groupByNumbers(nums);
+
+    }
+// adwsfasdfa sddf asdf asdf asdf
+    private static void groupByNumbers(int[] nums) {
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            if (!numsMap.containsKey(nums[i])) {
+                numsMap.put(nums[i], 1);
+            } else {
+                numsMap.put(nums[i], numsMap.get(nums[i]) + 1);
+            }
+        }
+        System.out.println(numsMap);
+    }
+}
